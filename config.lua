5f0bc2c0807c4d70b96b12e0b0e9ca80654ac8f7
postmorta1 <pezehk@gmail.com>
2017-08-24 22:35:39 +0000

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,22 @@
+Config              = {}
+Config.MarkerType   = 1
+Config.DrawDistance = 100.0
+Config.ZoneSize     = {x = 5.0, y = 5.0, z = 1.0}
+Config.MarkerColor  = {r = 100, g = 204, b = 100}
+Config.RequiredCopsCoke = 0
+Config.RequiredCopsMeth = 0
+Config.RequiredCopsWeed = 0
+Config.Locale = 'en'
+
+Config.Zones = {
+	CokeFarm = 	{x=1609.125, y=6663.5942382813, z=20.961572647095},
+	CokeTreatment = {x=-458.13967895508, y=-2278.6174316406, z=7.5158290863037},
+	CokeResell = 	{x=-1756.1984863281, y=427.31674194336, z=126.68292999268},
+	MethFarm = 	{x=709.0, y=4184.4213, z=39.62443},
+	MethTreatment = {x=1392.0, y=3608.0, z=38.0},
+	MethResell = 	{x=-63.592178344727, y=-1224.0709228516, z=27.768648147583},
+	WeedFarm = 	{x=2232.0, y=5582.0, z=53.0},
+	WeedTreatment = {x=91.061386108398, y=3750.0380859375, z=39.77326965332},
+	WeedResell = 	{x=141.0, y=-1921.0, z=20.123131},
+}
+
